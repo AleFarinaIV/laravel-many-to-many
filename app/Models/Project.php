44710e4659064135dfa2ba63fab842dd245fe116
Alessandro Farina <alessandrofarina401@gmail.com>
2024-10-13 21:59:21 +0200

    Initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'description', 
+        'start_date', 
+        'end_date', 
+        'stack',
+        'status',
+        'image_path',
+        'slug',
+        'type_id'
+    ];
+
+    public static function generateSlug($name) {
+        return Str::slug($name, '-');
+    }
+
+    public function type() {
+        return $this->belongsTo((Type::class));
+    }
+}
